7bb7bd516eaadf014036cd3c35eb0e32913c0a47
Pablo <paul.ladiv123@gmail.com>
2018-11-09 19:43:30 +0000

    first test for the Car class

--- lib/car.rb
+++ lib/car.rb
@@ -1,9 +1,23 @@
+require 'byebug'
+
 class Car
-  attr_reader :on_or_off
+  # attr_accessor :on_or_off
+
+  def initialize
+    @on_or_off = "off"
+  end
 
   def toggle
-    return "off" if self.on_or_off == "on"
-    return "on" if self.on_or_off == "off"
+    if @on_or_off == "on"
+      @on_or_off = "off"
+    else
+      @on_or_off = "on"
+    end
+    @on_or_off
+  end
+
+  def on_or_off
+    @on_or_off
   end
 
 end
--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -3,13 +3,16 @@ require('car')
 describe Car do
   context "#toggle_car" do
 
+    it "should start with OFF" do
+      expect(subject.on_or_off).to eq("off")
+    end
+
     it "if the car is off, you turn it on " do
-      allow(subject).to receive(:on_or_off).and_return("off")
       expect(subject.toggle).to eq("on")
     end
 
     it "if the car is on, you turn it off" do
-      allow(subject).to receive(:on_or_off).and_return("on")
+      subject.toggle
       expect(subject.toggle).to eq("off")
     end
 
